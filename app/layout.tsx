3b22263792503a44008f2c8f59a8fef97733d56e
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-05-13 16:24:01 +0300

    remove images folder from public

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     images:[
       {
       alt:"Website preview",
-      url: "Images/meta.jpg",
+      url: "/meta.jpg",
       width: 1200,
       height: 630,
     },
