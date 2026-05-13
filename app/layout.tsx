0493d163ef86472fb4ec0b1d2a00fac788f3cd56
Tetiana Kubyshkina <tanyasha_@ukr.net>
2026-05-13 16:19:03 +0300

    fix metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export const metadata: Metadata = {
     images:[
       {
       alt:"Website preview",
-      url: "/meta.jpg",
+      url: "Images/meta.jpg",
       width: 1200,
       height: 630,
     },
